89052f84ff2507d5a3e0c9250ca62973dea49cc2
rjk51 <ayaanrjk2004@gmail.com>
2024-02-07 00:01:47 +0530

    feat: Add Onboarding Page

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,56 +1,146 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:vishnu/authentication/auth_gate.dart';
 
+class OnBoardingPage extends StatefulWidget {
+  const OnBoardingPage({super.key});
 
+  @override
+  OnBoardingPageState createState() => OnBoardingPageState();
+}
 
-class SplashPage extends StatelessWidget {
-  const SplashPage({super.key});
+class OnBoardingPageState extends State<OnBoardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => const AuthGate()),
+    );
+  }
+
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('lib/assets/$assetName', width: width);
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Delay for 3 seconds
-    Future.delayed(const Duration(seconds: 5), () {
-      // Navigate to the home page
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const AuthGate(),
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = PageDecoration(
+        titleTextStyle: TextStyle(
+          fontSize: 28.0,
+          fontWeight: FontWeight.w700,
+        ),
+        titlePadding: EdgeInsets.only(top: 20.0, bottom: 25.0),
+        bodyTextStyle: bodyStyle,
+        bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+        pageColor: Colors.white,
+        imagePadding: EdgeInsets.only(
+          top: 90,
         ),
       );
-    });
 
-    // Placeholder widget for the splash screen UI
-    return Scaffold(
-      backgroundColor: Colors.yellow,
-      body: Center(
-        child: 
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'lib/assets/splash2.jpg',
-              width: 300,
-              height: 250,
-            ),
-         const SizedBox(height: 200),
-            const Align(
-            alignment: Alignment.bottomCenter,
-           
-            child: Text(
-              'powered by Grow Green',
-              style: TextStyle(
-                fontSize: 10,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 46, 107, 42),
-              ),
-            ),
-          )
-          ],
-          
-        
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: Colors.white,
+      allowImplicitScrolling: true,
+      autoScrollDuration: 3000,
+      infiniteAutoScroll: false,
+      globalFooter: SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.green,
+            foregroundColor: Colors.white,
+          ),
+          child: const Text(
+            'Get Started',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () => _onIntroEnd(context),
         ),
-
       ),
+      pages: [
+        PageViewModel(
+          title: "Real Time Prices",
+          body:
+              "Get Real time prices of crops. Get the best price for your produce.",
+          image: _buildImage('tractor.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Predict the prices",
+          body:
+              "Predict the prices of crops and make informed decisions.",
+          image: _buildImage('farmer1.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Simple and Easy to use",
+          body:
+              "Simple and easy to use interface. Get the best price for your produce.",
+          image: _buildImage('farmer2.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Connect with the community",
+          body:
+              "Connect and talk to other farmers. Share your experiences and learn from others.",
+          image: _buildImage('farmers.jpg'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "The best platform for farmers",
+          bodyWidget: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Click on the button to get started", style: bodyStyle),
+            ],
+          ),
+          decoration: pageDecoration.copyWith(
+            bodyFlex: 2,
+            imageFlex: 4,
+            bodyAlignment: Alignment.bottomCenter,
+            imageAlignment: Alignment.topCenter,
+          ),
+          image: _buildImage('farmer_mobile.jpg'),
+          reverse: true,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: false,
+      showNextButton: false,
+      skipOrBackFlex: 0,
+      nextFlex: 0,
+      showBackButton: false,
+      showDoneButton: false,
+      //rtl: true, // Display as right-to-left
+      back: const Icon(Icons.arrow_back),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.only(bottom: 25),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(12.0)
+          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator: const ShapeDecoration(
+        color: Colors.transparent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+      controlsPosition: const Position(bottom: 12.0, left: 16.0, right: 16.0),
     );
   }
 }
